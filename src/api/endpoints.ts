c069a098ca4c1cb9ba519369a59462f149058582
Wojciech Grabowski (PGS Software) <wgrabowski@pgs-soft.com>
2023-01-16 19:16:13 +0100

    feat: handle API errors

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,4 +1,4 @@
-const PGS_RADAR_API_URL = "https://radar.pgs-soft.com/api/";
+const PGS_RADAR_API_URL = "https://radar.pgs-soft.com/api";
 export const API_ENDPOINTS = {
 	RADARS: () => `${PGS_RADAR_API_URL}/radars`,
 	FEATURES: () => `${PGS_RADAR_API_URL}/radars/features`,
