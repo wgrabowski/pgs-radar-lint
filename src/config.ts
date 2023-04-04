c56c5a3209470ad32f8661dfbddecea392e74dcb
Wojciech Grabowski (PGS Software) <wgrabowski@pgs-soft.com>
2023-04-04 18:11:17 +0200

    chore: update feature flag name

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const API_URL = "https://radar.xebia.com/api";
 export const CONFIG_FILE_NAME = ".radarlintrc";
-export const NPM_FEATURE_FLAG_NAME = "NPM";
+export const NPM_FEATURE_FLAG_NAME = "PACKAGES";
 
 export const LINT_EXECUTABLE_NAME = "xebia-radar-lint";
